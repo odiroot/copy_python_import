9b8a59fc7dca9821a2d744bd1c4ae0cdaa8acd3e
Michal Odnous <odiroot@users.noreply.github.com>
2025-09-11 13:16:26 +0200

    More renaming

--- copy_python_import.py
+++ copy_python_import.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import os
 import sys
 
@@ -7,20 +5,19 @@ import sublime
 import sublime_plugin
 
 
-class CopyPythonPathCommand(sublime_plugin.TextCommand):
-
+class CopyPythonImportCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         python_path_items = []
         head, tail = os.path.split(self.view.file_name())
 
-        module = tail.rsplit('.', 1)[0]
-        if module != '__init__':
+        module = tail.rsplit(".", 1)[0]
+        if module != "__init__":
             python_path_items.append(module)
 
         head, tail = os.path.split(head)
 
         while tail:
-            if '__init__.py' in os.listdir(os.path.join(head, tail)):
+            if "__init__.py" in os.listdir(os.path.join(head, tail)):
                 python_path_items.insert(0, tail)
             else:
                 break
@@ -28,11 +25,15 @@ class CopyPythonPathCommand(sublime_plugin.TextCommand):
             head, tail = os.path.split(head)
 
         caret_point = self.view.sel()[0].begin()
-        class_entity = 'entity.name.class.python' if sys.version_info < (3, 0, 0) else 'entity.name.type.class.python'
+        class_entity = (
+            "entity.name.class.python"
+            if sys.version_info < (3, 0, 0)
+            else "entity.name.type.class.python"
+        )
         if class_entity in self.view.scope_name(caret_point):
             python_path_items.append(self.view.substr(self.view.word(caret_point)))
 
-        if 'entity.name.function.python' in self.view.scope_name(caret_point):
+        if "entity.name.function.python" in self.view.scope_name(caret_point):
             method_name = self.view.substr(self.view.word(caret_point))
             if self.view.indentation_level(caret_point) > 0:
                 regions = self.view.find_by_selector(class_entity)
@@ -49,11 +50,11 @@ class CopyPythonPathCommand(sublime_plugin.TextCommand):
 
             python_path_items.append(method_name)
 
-        python_path = '.'.join(python_path_items)
+        python_path = ".".join(python_path_items)
         sublime.set_clipboard(python_path)
 
         sublime.status_message('"%s" copied to clipboard' % python_path)
 
     def is_enabled(self):
-        matcher = 'source.python'
+        matcher = "source.python"
         return self.view.match_selector(self.view.sel()[0].begin(), matcher)
