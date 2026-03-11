92743512543096aaa2b9ec41f8043e7a4b2184f9
Michal Odnous <odiroot@users.noreply.github.com>
2026-03-11 11:55:04 +0100

    Fix import edge cases.
    - Class selection import.
    - Class method selection indirect import.

--- copy_python_import.py
+++ copy_python_import.py
@@ -1,8 +1,13 @@
 import os
-import sys
 
 import sublime
 import sublime_plugin
+
+CLASS_NAME_SELECTORS = (
+    "entity.name.class.python",
+    "entity.name.type.class.python",
+)
+FUNCTION_NAME_SELECTOR = "entity.name.function.python"
 
 
 def _get_module_path(file_name):
@@ -31,6 +36,49 @@ def _get_module_path(file_name):
 
 
 class CopyPythonImportCommand(sublime_plugin.TextCommand):
+    def _matches_any_selector(self, point, selectors):
+        return any(
+            self.view.match_selector(point, selector)
+            for selector in selectors
+        )
+
+    def _find_regions_for_any_selector(self, selectors):
+        regions = []
+        seen = set()
+
+        for selector in selectors:
+            for region in self.view.find_by_selector(selector):
+                key = (region.a, region.b)
+                if key in seen:
+                    continue
+                seen.add(key)
+                regions.append(region)
+
+        return sorted(regions, key=lambda region: region.a)
+
+    def _find_containing_class_name(self, point):
+        class_name_regions = self._find_regions_for_any_selector(
+            CLASS_NAME_SELECTORS
+        )
+        possible_class_point = 0
+
+        for region in class_name_regions:
+            if region.a < point:
+                possible_class_point = region.a
+            else:
+                break  # We've gone past the cursor
+
+        if possible_class_point <= 0:
+            return None
+
+        # Guess: if cursor is more indented than the class, it's inside.
+        class_indent = self.view.indentation_level(possible_class_point)
+        cursor_indent = self.view.indentation_level(point)
+        if cursor_indent <= class_indent:
+            return None
+
+        return self.view.substr(self.view.word(possible_class_point))
+
     def run(self, edit):
         # Get the base module path from the file's location.
         module_path = _get_module_path(self.view.file_name())
@@ -44,15 +92,27 @@ class CopyPythonImportCommand(sublime_plugin.TextCommand):
 
         module_part = ".".join(module_path)
 
-        if symbol_path:
-            symbol_part = ".".join(symbol_path)
-            statement = "from %s import %s" % (module_part, symbol_part)
-        else:
-            statement = "import %s" % (module_part)
+        statement = self._build_statement(module_part, symbol_path)
 
         # Copy to clipboard and show status.
         sublime.set_clipboard(statement)
         sublime.status_message('"%s" copied to clipboard' % statement)
+
+    def _build_statement(self, module_part, symbol_path):
+        if not symbol_path:
+            return "import %s" % (module_part,)
+
+        if len(symbol_path) == 2:
+            class_name, method_name = symbol_path
+            return "from %s import %s\n%s.%s" % (
+                module_part,
+                class_name,
+                class_name,
+                method_name,
+            )
+
+        symbol_part = ".".join(symbol_path)
+        return "from %s import %s" % (module_part, symbol_part)
 
     def _get_symbol_path_items(self):
         """
@@ -60,31 +120,15 @@ class CopyPythonImportCommand(sublime_plugin.TextCommand):
         containing the cursor.
         """
         caret_point = self.view.sel()[0].begin()
-        scope = self.view.scope_name(caret_point)
         path_items = []
 
-        class_entity = (
-            "entity.name.class.python"
-            if sys.version_info < (3, 0, 0)
-            else "entity.name.type.class.python"
-        )
-
         # Priority 1: Cursor is on a function/method name.
-        if "entity.name.function.python" in scope:
+        if self.view.match_selector(caret_point, FUNCTION_NAME_SELECTOR):
             method_name = self.view.substr(self.view.word(caret_point))
             if self.view.indentation_level(caret_point) > 0:
-                regions = self.view.find_by_selector(class_entity)
-                possible_class_point = 0
-                for region in regions:
-                    if region.b < caret_point:
-                        possible_class_point = region.a
-                    else:
-                        break
-                if possible_class_point > 0:
-                    class_name = self.view.substr(
-                        self.view.word(possible_class_point)
-                    )
-                    path_items.append(class_name)
+                class_name = self._find_containing_class_name(caret_point)
+                if class_name:
+                    path_items.extend((class_name, method_name))
                 else:
                     pass  # Nested function, do nothing.
             else:
@@ -92,31 +136,15 @@ class CopyPythonImportCommand(sublime_plugin.TextCommand):
             return path_items
 
         # Priority 2: Cursor is directly on a class name.
-        if class_entity in scope:
+        if self._matches_any_selector(caret_point, CLASS_NAME_SELECTORS):
             path_items.append(self.view.substr(self.view.word(caret_point)))
             return path_items
 
         # Priority 3: Try to find a containing class.
-        # Find the last class definition that starts before the cursor.
-        class_name_regions = self.view.find_by_selector(class_entity)
-        possible_class_point = 0
-        for region in class_name_regions:
-            if region.a < caret_point:
-                possible_class_point = region.a
-            else:
-                break  # We've gone past the cursor
-
-        if possible_class_point > 0:
-            # Guess: if cursor is more indented than the class, it's inside.
-            class_indent = self.view.indentation_level(possible_class_point)
-            cursor_indent = self.view.indentation_level(caret_point)
-
-            if cursor_indent > class_indent:
-                class_name = self.view.substr(
-                    self.view.word(possible_class_point)
-                )
-                path_items.append(class_name)
-                return path_items
+        class_name = self._find_containing_class_name(caret_point)
+        if class_name:
+            path_items.append(class_name)
+            return path_items
 
         return path_items
 
